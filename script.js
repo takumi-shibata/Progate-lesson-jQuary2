212322df48ef8aa19dfe7862f3107b6ede622fc0
takumi-shibata <241448ta@gmail.com>
2020-07-27 15:01:40 +0900

    1周目モーダル終了

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(function() {
-  
+  // ログインclickイベント(モーダルの表示)
+  $('#login-show').click(function() {
+    $('#login-modal').fadeIn();
+  });
+
+  // サインアップclickイベント(モーダルの表示)
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  // (ログイン・サインアップ時)モーダル内閉じるボタンclickイベント
+  $('.close-modal').click(function() {
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
 });
