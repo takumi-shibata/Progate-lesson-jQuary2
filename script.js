f5778a1ba952a7aa3240362e780ad59ca0a70319
takumi-shibata <241448ta@gmail.com>
2020-07-27 17:13:53 +0900

    1周目hoverイベント終了

--- script.js
+++ script.js
@@ -14,4 +14,18 @@ $(function() {
     $('#login-modal').fadeOut();
     $('#signup-modal').fadeOut();
   });
+
+  // レッスン一覧のhoverイベント
+  $('.lesson-hover').hover(
+    function() {
+     // カーソルが乗った時の処理(文字の表示 CSS「text-active」)
+     // addClassメソッド: 指定した要素(text-contents)に、クラス(text-active)をjQeury側で追加することができる
+     $(this).find('.text-contents').addClass('text-active');
+    },
+    function() {
+     // カーソルが外れた時の処理(文字の非表示 CSS「text-contents」)
+     // removeClassメソッド: 指定した要素(text-contents)から、追加したクラス(text-active)をjQeury側で削除することができる
+     $(this).find('.text-contents').removeClass('text-active');
+    }
+  );
 });
