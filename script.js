3f5ddbde3bba6ca2486f783725e1a843f17c28f1
takumi-shibata <241448ta@gmail.com>
2020-07-29 14:21:35 +0900

    1周目学習終了(js一部修正)

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ $(function() {
   $('.lesson-hover').hover(
     function() {
      // カーソルが乗った時の処理(文字の表示 CSS「text-active」)
-     // addClassメソッド: 指定した要素(text-contents)に、クラス(text-active)をjQeury側で追加することができる
+     // addClassメソッド: 指定した要素(text-contents)に、クラス(text-active)をjQeury側で追加することができる。※addClassメソッドの引数(追加するクラス)に「.」はつけない！
      $(this).find('.text-contents').addClass('text-active');
     },
     function() {
      // カーソルが外れた時の処理(文字の非表示 CSS「text-contents」)
-     // removeClassメソッド: 指定した要素(text-contents)から、追加したクラス(text-active)をjQeury側で削除することができる
+     // removeClassメソッド: 指定した要素(text-contents)から、追加したクラス(text-active)をjQeury側で削除することができる。※removeClassメソッドの引数(削除するクラス)に「.」はつけない！
      $(this).find('.text-contents').removeClass('text-active');
     }
   );
